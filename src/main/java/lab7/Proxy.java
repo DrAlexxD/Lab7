aa800caefa1382989264f8b1caac1c098dc0dbe0
DrAlexxD <alexander.drugakov@mail.ru>
2019-12-27 22:16:17 +0300

    Scripted auto-commit on change (2019-12-27 22:16:17) by gitwatch.sh

--- src/main/java/lab7/Proxy.java
+++ src/main/java/lab7/Proxy.java
@@ -20,4 +20,18 @@ public class Proxy {
         poller.register(toCache, ZMQ.Poller.POLLIN);
 
     }
+
+    private void proxyInitialization() {
+        ZContext context = new ZContext();
+        ZMQ.Socket toCache = context.createSocket(SocketType.ROUTER);
+        ZMQ.Socket toClient = context.createSocket(SocketType.ROUTER);
+        toCache.setHWM(0);
+        toClient.setHWM(0);
+        toCache.bind(CacheStorage.DEALER_SOCKET);
+        toClient.bind(Client.CLIENT_SOCKET);
+
+        ZMQ.Poller poller = context.createPoller(2);
+        poller.register(toClient, ZMQ.Poller.POLLIN);
+        poller.register(toCache, ZMQ.Poller.POLLIN);
+    }
 }
