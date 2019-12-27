7bede677399ecec2bb881fe1e176a2820fbac770
DrAlexxD <alexander.drugakov@mail.ru>
2019-12-27 21:40:20 +0300

    Scripted auto-commit on change (2019-12-27 21:40:19) by gitwatch.sh

--- src/main/java/lab7/CacheStorage.java
+++ src/main/java/lab7/CacheStorage.java
@@ -1,4 +1,13 @@
 package lab7;
 
+import java.util.Scanner;
+
 public class CacheStorage {
+    private static int leftBorder, rightBorder;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        leftBorder = in.nextInt();
+        rightBorder = in.nextInt();
+    }
 }
