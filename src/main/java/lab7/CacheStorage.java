941eeaa68446486a39b2638e471d2811bf146ad7
DrAlexxD <alexander.drugakov@mail.ru>
2019-12-27 22:01:19 +0300

    Scripted auto-commit on change (2019-12-27 22:01:18) by gitwatch.sh

--- src/main/java/lab7/CacheStorage.java
+++ src/main/java/lab7/CacheStorage.java
@@ -3,6 +3,7 @@ package lab7;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +12,9 @@ import java.util.Scanner;
 public class CacheStorage {
     private static int leftBorder, rightBorder;
     public static final String DEALER = "tcp://localhost:2050";
-
+    public static final int TIMEOUT = 5000;
+    public static final String HEARTBEAT = "Heartbleed";
+    public static final String SPACE_DELIMITER = " ";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -31,6 +34,14 @@ public class CacheStorage {
         dealer.connect(DEALER);
         ZMQ.Poller poller = context.createPoller(1);
         poller.register(dealer, ZMQ.Poller.POLLIN);
-        
+        long startTime = System.currentTimeMillis();
+        while (!Thread.currentThread().isInterrupted()) {
+            poller.poll(1);
+            if (System.currentTimeMillis() - startTime > TIMEOUT) {
+                ZMsg m = new ZMsg();
+                m.addLast(HEARTBEAT + SPACE_DELIMITER + leftBorder + SPACE_DELIMITER + rightBorder);
+                m.send(dealer);
+            }
+        }
     }
 }
